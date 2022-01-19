60716a07f79004fb406e6b776141cfeefdc0a1be
Nandhanan10 <90860935+Nandhanan10@users.noreply.github.com>
2022-01-19 13:32:50 +0530

    Add files via upload

---
+++ module 2 N/nandhanan 6.py
@@ -0,0 +1,9 @@
+name = "Graphic Designer"
+age = 20
+a = 89.99
+is_experienced = True
+b = 2
+items = ['HCl', 'L&T', 'TVS', 'Vj studio']
+print("Nandhanan R", "\nAge : %s \nProfession : %s \nExperienced : %s \nNo.of years of experience : %s"
+                     "\nProject for leading companies like : %s \nCostumer satisfaction in : %s" %\
+     (age, name, is_experienced, b, items, a))
---
+++ module 2 N/nandhanan.py
@@ -0,0 +1,9 @@
+name = "Graphic Designer"
+age = 20
+a = 89.99
+is_experienced = True
+b = 2
+items = ['HCl', 'L&T', 'TVS', 'Vj studio']
+print("Nandhanan R", "\nAge : {} \nProfession : {} \nExperienced : {} \nNo.of years of experience : {}"
+                     "\nProject for leading companies like : {} \nCostumer satisfaction in : {}" \
+      .format (age, name, is_experienced, b, items, a))
